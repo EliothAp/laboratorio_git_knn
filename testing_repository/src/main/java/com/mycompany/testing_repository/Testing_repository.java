4f1f9848421b19beb9709e70ac70687978f4b527
EliothAp <eartavia70064@ufide.ac.cr>
2025-09-16 19:35:16 -0600

    Base de mi proyecto

---
+++ testing_repository/src/main/java/com/mycompany/testing_repository/Testing_repository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing_repository;
+
+/**
+ *
+ * @author Elioth Artavia
+ */
+public class Testing_repository {
+
+    public static void main(String[] args) {
+    
+        int numero = 7;
+        
+        System.out.println("Soy un numero" + numero);
+    }
+}
